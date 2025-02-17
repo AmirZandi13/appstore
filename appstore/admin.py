349fc305d8bfc86e02e567179351cb11a25bdcb8
Amir Zandieh <amirzandieh1@gmail.com>
2025-02-18 00:59:15 +0330

    feat(database): implement model and admin panel

--- appstore/admin.py
+++ appstore/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from appstore.models import App
 
-# Register your models here.
+@admin.register(App)
+class AppAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'price', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('title', 'owner__username')
+
+    actions = ['verify_apps']
+
+    def verify_apps(self, request, queryset):
+        queryset.update(status='verified')
+    verify_apps.short_description = "Mark selected apps as verified"
